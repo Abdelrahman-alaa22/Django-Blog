8e19ab016304b5b7241f9b80bee6f4d80278e79d
Abdelrahman-alaa22 <Abdelrahman.alaa4497@Gmail.com>
2020-05-04 04:34:39 +0200

    Home page generic listview for each post created

--- Django_Blog/Postapp/views.py
+++ Django_Blog/Postapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Post
+from django.contrib.auth.models import User
 # Create your views here.
 
 
+class Home(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'Postapp/home.html'
 
 
 
