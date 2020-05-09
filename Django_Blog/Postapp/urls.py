5de5c783f061c038d042da4c191672186e230d71
Abdelrahman-alaa22 <Abdelrahman.alaa4497@Gmail.com>
2020-05-09 02:27:49 +0200

    user posts list

--- Django_Blog/Postapp/urls.py
+++ Django_Blog/Postapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Home, DetailPost, PostCreate, UpdatePost, DeletePost
+from .views import Home, DetailPost, PostCreate, UpdatePost, DeletePost, UserPostList
 from . import views
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('user/<str:username>', UserPostList.as_view(), name='user-posts'),
     path('post/<int:pk>', DetailPost.as_view(), name='post-details'),
     path('create/', PostCreate.as_view(), name='createpost'),
     path('post/<int:pk>/update/', UpdatePost.as_view(), name='updateview'),
--- Django_Blog/Postapp/views.py
+++ Django_Blog/Postapp/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 # Create your views here.
 
 class Home(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'Postapp/home.html'
+    ordering = '-date_posted'
+
+
+
+class UserPostList(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'Postapp/user_post.html'
+    ordering = '-date_posted'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 
 def about(request):
-
     return render(request, template_name='Postapp/about.html')
 
 
